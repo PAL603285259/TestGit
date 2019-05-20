0ebb83614cb6a9460eb8a3e3bb4a0aa9ec3d9bbb
PAL <603285259@qq.com>
2019-05-21 00:47:42 +0800

    屏蔽while函数中的所有代码

--- 实验4 串口实验/USER/main.c
+++ 实验4 串口实验/USER/main.c
@@ -28,29 +28,29 @@
 	KEY_Init();          //初始化与按键连接的硬件接口
  	while(1)
 	{
-		if(USART_RX_STA&0x8000)
-		{					   
-			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
-			printf("\r\n您发送的消息为:\r\n\r\n");
-			for(t=0;t<len;t++)
-			{
-				USART_SendData(USART1, USART_RX_BUF[t]);//向串口1发送数据
-				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
-			}
-			printf("\r\n\r\n");//插入换行
-			USART_RX_STA=0;
-		}else
-		{
-			times++;
-			if(times%5000==0)
-			{
-				printf("\r\n战舰STM32开发板 串口实验\r\n");
-				printf("正点原子@ALIENTEK\r\n\r\n");
-			}
-			if(times%200==0)printf("请输入数据,以回车键结束\n");  
-			if(times%30==0)LED0=!LED0;//闪烁LED,提示系统正在运行.
-			delay_ms(10);   
-		}
+//		if(USART_RX_STA&0x8000)
+//		{					   
+//			len=USART_RX_STA&0x3fff;//得到此次接收到的数据长度
+//			printf("\r\n您发送的消息为:\r\n\r\n");
+//			for(t=0;t<len;t++)
+//			{
+//				USART_SendData(USART1, USART_RX_BUF[t]);//向串口1发送数据
+//				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//等待发送结束
+//			}
+//			printf("\r\n\r\n");//插入换行
+//			USART_RX_STA=0;
+//		}else
+//		{
+//			times++;
+//			if(times%5000==0)
+//			{
+//				printf("\r\n战舰STM32开发板 串口实验\r\n");
+//				printf("正点原子@ALIENTEK\r\n\r\n");
+//			}
+//			if(times%200==0)printf("请输入数据,以回车键结束\n");  
+//			if(times%30==0)LED0=!LED0;//闪烁LED,提示系统正在运行.
+//			delay_ms(10);   
+//		}
 	}	 
  }
 
